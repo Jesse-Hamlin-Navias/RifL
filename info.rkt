b7f20db9fbf697a78bf8617c3f2994ae3b83f198
Jesse Hamlin-Navias <107646663+Jesse-Hamlin-Navias@users.noreply.github.com>
2023-03-29 19:28:22 -0400

    img-try and version number update

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define collection "RifL")
-(define version "1.0")
+(define version "1.01")
 (define scribblings '(("scribblings/RifL.scrbl")))
 (define deps '("base"
                "beautiful-racket-lib"
